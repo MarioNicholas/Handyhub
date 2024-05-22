648b6f8c6bbb86db01ea51108e674fc245c5fa4b
MarioNicholas <18221061@std.stei.itb.ac.id>
2024-05-22 22:58:20 +0700

    Authentication API

--- frontend/app/(auth)/register.tsx
+++ frontend/app/(auth)/register.tsx
@@ -1,90 +1,143 @@
-import { Link } from 'expo-router';
-import * as React from 'react'
+import { Link } from "expo-router";
+import * as React from "react";
 import { View, Image, ScrollView } from "react-native";
-import { Text, TextInput, Button } from 'react-native-paper'
+import { Text, TextInput, Button } from "react-native-paper";
 
 export default function register() {
-    const logo = require("@/assets/images/splash.png")
-    return (
-        <View
+  const logo = require("@/assets/images/splash.png");
+  return (
+    <View
+      style={{
+        backgroundColor: "#027361",
+        height: "100%",
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "flex-start",
+        paddingBottom: 90,
+      }}
+    >
+      <Image
+        source={logo}
         style={{
-            backgroundColor: '#027361',
-            height: '100%',
-            display: 'flex',
-            alignItems: 'center',
-            justifyContent: 'flex-start',
-            paddingBottom: 90
+          width: 200,
+          height: 200,
         }}
+      />
+      <ScrollView
+        style={{
+          backgroundColor: "#fff",
+          width: "70%",
+          borderWidth: 1,
+          borderRadius: 16,
+          borderColor: "#000",
+          height: "60%",
+        }}
+        contentContainerStyle={{
+          display: "flex",
+          alignItems: "center",
+        }}
+      >
+        <Text
+          variant="headlineLarge"
+          style={{
+            fontWeight: "bold",
+            marginTop: 20,
+            marginBottom: 30,
+            textAlign: "center",
+          }}
         >
-            <Image source={logo} style={{
-                width: 200,
-                height: 200
-            }}/>
-            <ScrollView style={{
-                backgroundColor: '#fff',
-                width: '70%',
-                borderWidth: 1,
-                borderRadius: 16,
-                borderColor: '#000',
-                height: '60%',
-            }}
-                contentContainerStyle={{
-                    display: "flex",
-                    alignItems: "center"
-                }}
-            >
-                <Text variant="headlineLarge" style={{fontWeight: 'bold', marginTop: 20, marginBottom: 30, textAlign: 'center'}}>Register</Text>
-                <TextInput label={"Full Name"} style={{
-                    backgroundColor: "#fff",
-                    borderRadius: 10,
-                    marginHorizontal: 10,
-                    marginVertical: 10,
-                    width: "90%"
-                }}
-                mode="outlined"/>
-                <TextInput label={"Email"} style={{
-                    backgroundColor: "#fff",
-                    borderRadius: 10,
-                    marginHorizontal: 10,
-                    marginVertical: 10,
-                    width: "90%"
-                }}
-                mode="outlined"/>
-                <TextInput label={"Phone Number"} style={{
-                    backgroundColor: "#fff",
-                    borderRadius: 10,
-                    marginHorizontal: 10,
-                    marginVertical: 10,
-                    width: "90%"
-                }}
-                mode="outlined"/>
-                <TextInput label={"Address"} style={{
-                    backgroundColor: "#fff",
-                    borderRadius: 10,
-                    marginHorizontal: 10,
-                    marginVertical: 10,
-                    width: "90%"
-                }}
-                mode="outlined"/>
-                <TextInput label={"Username"} style={{
-                    backgroundColor: "#fff",
-                    borderRadius: 10,
-                    marginHorizontal: 10,
-                    marginVertical: 10,
-                    width: "90%"
-                }}
-                mode="outlined"/>
+          Register
+        </Text>
+        <TextInput
+          label={"Full Name"}
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            marginHorizontal: 10,
+            marginVertical: 10,
+            width: "90%",
+          }}
+          mode="outlined"
+        />
+        <TextInput
+          label={"Email"}
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            marginHorizontal: 10,
+            marginVertical: 10,
+            width: "90%",
+          }}
+          mode="outlined"
+        />
+        <TextInput
+          label={"Phone Number"}
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            marginHorizontal: 10,
+            marginVertical: 10,
+            width: "90%",
+          }}
+          mode="outlined"
+        />
+        <TextInput
+          label={"Address"}
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            marginHorizontal: 10,
+            marginVertical: 10,
+            width: "90%",
+          }}
+          mode="outlined"
+        />
+        <TextInput
+          label={"Username"}
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            marginHorizontal: 10,
+            marginVertical: 10,
+            width: "90%",
+          }}
+          mode="outlined"
+        />
 
-                <Button mode="contained" style={{
-                    backgroundColor: "#027361",
-                    width: "80%",
-                    marginVertical: 20
-                }} onPress={() => console.log('Pressed')}>
-                    Register
-                </Button>
-                <Text variant="bodySmall" style={{ marginTop: 20, textAlign: 'center', paddingBottom: 30}}>Already have an account?<Link href={"/login"} style={{ fontWeight: 'bold'}}> Login Here</Link></Text>
-                
-            </ScrollView>
-        </View>
-    )
+        <TextInput
+          label={"Password"}
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: 10,
+            marginHorizontal: 10,
+            marginVertical: 10,
+            width: "90%",
+          }}
+          mode="outlined"
+        />
+
+        <Button
+          mode="contained"
+          style={{
+            backgroundColor: "#027361",
+            width: "80%",
+            marginVertical: 20,
+          }}
+          onPress={() => console.log("Pressed")}
+        >
+          Register
+        </Button>
+        <Text
+          variant="bodySmall"
+          style={{ marginTop: 20, textAlign: "center", paddingBottom: 30 }}
+        >
+          Already have an account?
+          <Link href={"/login"} style={{ fontWeight: "bold" }}>
+            {" "}
+            Login Here
+          </Link>
+        </Text>
+      </ScrollView>
+    </View>
+  );
 }
--- frontend/app/(customer)/index.tsx
+++ frontend/app/(customer)/index.tsx
@@ -1,12 +1,13 @@
 import { Link } from "expo-router";
 import { View, Image, ScrollView } from "react-native";
-import * as React from 'react';
-import { Text } from 'react-native-paper'
+import * as React from "react";
+import { Text } from "react-native-paper";
 import Card from "@/components/Card";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function Index() {
-  const Hero = require("@/assets/images/banner.png")
-  const FeaturedPlaceholder = require("@/assets/images/featured-placeholder.png")
+  const Hero = require("@/assets/images/banner.png");
+  const FeaturedPlaceholder = require("@/assets/images/featured-placeholder.png");
   const electronics = require("@/assets/images/electronics.png");
   const plumbing = require("@/assets/images/plumbing.png");
   const cleaning = require("@/assets/images/cleaning.png");
@@ -15,7 +16,11 @@ export default function Index() {
   const relocation = require("@/assets/images/relocation.png");
   const others = require("@/assets/images/others.png");
   const apply = require("@/assets/images/apply.png");
-  
+
+  const [token, setToken] = React.useState<string | null>(null);
+  const [userId, setUserId] = React.useState<string | null>(null);
+  const [isAuth, setIsAuth] = React.useState<boolean>(false);
+
   const services = [
     { name: "electronics", src: electronics },
     { name: "plumbing", src: plumbing },
@@ -24,8 +29,130 @@ export default function Index() {
     { name: "gardening", src: gardening },
     { name: "relocation", src: relocation },
     { name: "others", src: others },
-    { name: "apply", src: apply }
+    { name: "apply", src: apply },
   ];
+
+  interface loginData {
+    username: string;
+    password: string;
+  }
+
+  interface signupData {
+    name: string;
+    email: string;
+    phoneNumber: string;
+    address: string;
+    username: string;
+    password: string;
+  }
+
+  const tokenChecker = async () => {
+    const token = await AsyncStorage.getItem("token");
+    const expiryDate = await AsyncStorage.getItem("expiryDate");
+
+    if (!token || !expiryDate) {
+      return;
+    }
+
+    if (new Date(expiryDate) <= new Date()) {
+      //logout
+      return;
+    }
+
+    const userId = await AsyncStorage.getItem("userId");
+    const remainingMiliSeconds =
+      new Date(expiryDate).getTime() - new Date().getTime();
+    setToken(token);
+    setUserId(userId);
+    setAutoLogout(remainingMiliSeconds);
+    setIsAuth(true);
+  };
+
+  React.useEffect(() => {
+    tokenChecker();
+  }, []);
+
+  const logoutHandler = async () => {
+    await AsyncStorage.removeItem("token");
+    await AsyncStorage.removeItem("userId");
+    await AsyncStorage.removeItem("expiryDate");
+    setToken(null);
+    setUserId(null);
+    setIsAuth(false);
+  };
+
+  const setAutoLogout = (millisecond: number) => {
+    setTimeout(() => {
+      logoutHandler();
+    }, millisecond);
+  };
+
+  const loginHandler = async (authData: loginData) => {
+    try {
+      const response = await fetch("http://localhost:8000/auth/login", {
+        method: "POST",
+        body: JSON.stringify({
+          username: authData.username,
+          password: authData.password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      if (response.status === 422) {
+        throw new Error();
+      }
+
+      if (response.status !== 200 && response.status !== 201) {
+        throw new Error();
+      }
+
+      const result = await response.json();
+      setToken(result.token);
+      setUserId(result.userId);
+      await AsyncStorage.setItem("token", result.token);
+      await AsyncStorage.setItem("userId", result.userId);
+      const remainingMilisecond = 60 * 60 * 1000;
+      const expiryDate = new Date().getTime() + remainingMilisecond;
+      await AsyncStorage.setItem("expiryDate", expiryDate.toString());
+      setAutoLogout(remainingMilisecond);
+      setIsAuth(true);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const signupHandler = async (authData: signupData) => {
+    try {
+      const response = await fetch("http://localhost:8000/auth/signup", {
+        method: "PUT",
+        body: JSON.stringify({
+          email: authData.email,
+          name: authData.name,
+          phoneNumber: authData.phoneNumber,
+          address: authData.address,
+          username: authData.username,
+          password: authData.password,
+        }),
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+      if (response.status === 422) {
+        throw new Error();
+      }
+
+      if (response.status !== 200 && response.status !== 201) {
+        throw new Error();
+      }
+
+      const result = await response.json();
+      setIsAuth(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
   return (
     <ScrollView
@@ -34,38 +161,64 @@ export default function Index() {
         flexGrow: 1,
       }}
     >
-      <Text variant="headlineLarge" style={{fontWeight: 'bold', marginTop: 20, marginBottom: 10}}>Welcome!</Text>
-      <View style={{
-        display:'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-      }}>
-        <Image source={Hero} style={{width: 330, height: 170}}/>
+      <Text
+        variant="headlineLarge"
+        style={{ fontWeight: "bold", marginTop: 20, marginBottom: 10 }}
+      >
+        Welcome!
+      </Text>
+      <View
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          width: "100%",
+        }}
+      >
+        <Image source={Hero} style={{ width: 330, height: 170 }} />
       </View>
-      <Text variant="headlineLarge" style={{fontWeight: 'bold', marginTop: 20, marginBottom: 10}}>Services</Text>
-      
-      <View style={{
-        display:'flex',
-        flex: 4,
-        justifyContent: "center",
-        flexDirection: 'row',
-        marginHorizontal: "auto",
-        width: "100%",
-        flexWrap: "wrap",
-      }}>
-        {services.map((service)=> {
+      <Text
+        variant="headlineLarge"
+        style={{ fontWeight: "bold", marginTop: 20, marginBottom: 10 }}
+      >
+        Services
+      </Text>
+
+      <View
+        style={{
+          display: "flex",
+          flex: 4,
+          justifyContent: "center",
+          flexDirection: "row",
+          marginHorizontal: "auto",
+          width: "100%",
+          flexWrap: "wrap",
+        }}
+      >
+        {services.map((service) => {
           return (
-            <View key={service.name} style={{margin: 8}}>
-              <Image source={service.src} style={{width: 62, height: 69}}/>
+            <View key={service.name} style={{ margin: 8 }}>
+              <Image source={service.src} style={{ width: 62, height: 69 }} />
             </View>
-          )
+          );
         })}
       </View>
-      
-      <Text variant="headlineLarge" style={{fontWeight: 'bold', marginTop: 20, marginBottom: 10}}>Featured</Text>
-      <Card name="Alan Smith" image={FeaturedPlaceholder} role="Hedge Specialist" rating={4.9} jobs={120} price={120000}/>
-      
+
+      <Text
+        variant="headlineLarge"
+        style={{ fontWeight: "bold", marginTop: 20, marginBottom: 10 }}
+      >
+        Featured
+      </Text>
+      <Card
+        name="Alan Smith"
+        image={FeaturedPlaceholder}
+        role="Hedge Specialist"
+        rating={4.9}
+        jobs={120}
+        price={120000}
+      />
+
       <Link href={"/login"}>Login</Link>
     </ScrollView>
   );
